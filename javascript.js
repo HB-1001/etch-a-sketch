f482dba51217d458e8c74af1f4d8d66d5c8b9f51
HB-1001 <75037684+HB-1001@users.noreply.github.com>
2021-02-19 23:19:47 +0400

    edit 2

--- javascript.js
+++ javascript.js
@@ -2,10 +2,13 @@
 let gridContainer = document.body.querySelector(".container");
 console.log(gridContainer);
 
-// defining i as a global scope for it to be accessible by buttons
+//making default start of grid 16x16 at beginning
+const gridInput = document.body.querySelector(".gridInput");
 
-let i = 0;
-let x = 256;
+// defining i as a global scope for it to be accessible by buttons
+let i = "";
+let x = "";
+
 
 //creating grids using loop
 const gridsetup = function () {
@@ -16,52 +19,48 @@ const gridsetup = function () {
     gridContainer.appendChild(gridBox);
   }
 };
-gridsetup();
 
 //color of mouseover
 let mouseoverColor = "black";
 
 // eventlistener for hover/mouseover
-// const mouseOver not working because it was using old grids Array, which was erased - can we make this a function?
+// const mouseOver not working because it was using old grids Array, which was erased -
 let grids = document.body.querySelectorAll(".grid");
-grids.forEach((item) =>
-  item.addEventListener("mouseover", function (e) {
-    e.target.style.backgroundColor = mouseoverColor;
-  })
-);
+let colorizer = function () {
+  grids = document.body.querySelectorAll(".grid");
+  grids.forEach((item) =>
+    item.addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = mouseoverColor;
+    })
+  );
+};
 
 // canvas size buttons / try repeat 100% to fill whole box
 const gridDimension = document.body.querySelectorAll(".canvasSize");
 gridDimension.forEach((canvas) =>
   canvas.addEventListener("click", function (e) {
-    grids.forEach((item, index, object) => item.parentNode.removeChild(item));
-    gridContainer.style.gridTemplateColumns = `repeat(${e.target.id}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${e.target.id}, 1fr)`;
-    x = `${e.target.id * e.target.id}`;
-    gridsetup();
-    grids = document.body.querySelectorAll(".grid");
-    grids.forEach((item) =>
-      item.addEventListener("mouseover", function (e) {
-        e.target.style.backgroundColor = mouseoverColor;
-      })
-    );
+    gridInput.value = e.target.id;
+    gridInput.dispatchEvent(new Event("change"));
   })
 );
 
 // //Cleanup, erase all button
-let cleaner = document.body.querySelector('.cleanUp')
-cleaner.addEventListener('click', function(e) {
-  grids.forEach(item => item.style.backgroundColor="white");
-})
+let cleaner = document.body.querySelector(".cleanUp");
+cleaner.addEventListener("click", function (e) {
+  grids.forEach((item) => (item.style.backgroundColor = "white"));
+});
 
-// //user prompt for grid size
-// prompt/input launches function :
-// grids.forEach((item, index, object) => item.parentNode.removeChild(item));
-// gridContainer.style.gridTemplateColumns = `repeat(${e.target.id}, 1fr)`;
-// gridContainer.style.gridTemplateRows = `repeat(${e.target.id}, 1fr)`;
-// x = `${e.target.id * e.target.id}`;
-// gridsetup();
-// grids = document.body.querySelectorAll(".grid");
-// grids.forEach((item) =>
-//   item.addEventListener("mouseover", function (e) {
-//     e.target.style.backgroundColor = mouseoverColor;
+// //user input for grid size / box size changer uses this too/
+gridInput.addEventListener("change", function (e) {
+  grids.forEach((item, index, object) => item.parentNode.removeChild(item));
+  gridContainer.style.gridTemplateColumns = `repeat(${e.target.value}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${e.target.value}, 1fr)`;
+  x = `${e.target.value * e.target.value}`;
+  gridsetup();
+  colorizer();
+});
+
+let startValue = 16;
+const startGrid = () => (gridInput.value = startValue);
+gridInput.dispatchEvent(new Event("change"));
+startGrid();
